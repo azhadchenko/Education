737d96caaf9f921a9d09a73fcef4d1a7da82ef5b
azhadchenko <bochka_tz@list.ru>
2016-05-16 01:06:22 +0300

    Completed inverted index without tests

--- course-software/task4/mmapedfile/inverted_index.c
+++ course-software/task4/mmapedfile/inverted_index.c
@@ -66,7 +66,7 @@ ssize_t destruct_manager(struct Ii_manager* manager) {
     return 0;
 }
 
-struct Ii_element* allocate_element(struct Ii_manager)* manager){
+struct Ii_element* allocate_element(struct Ii_manager* manager){
     if(!manager)
         return 0;
 
@@ -113,9 +113,9 @@ struct Ii_element* allocate_element(struct Ii_manager)* manager){
     manager -> data[index] = tmp;
     manager -> block_count++;
 
-    free(free_candidate); //Actually this can be a real problem if someone is handling previous pool -> data
+    free(free_candidate); //Actually this can be a real problem if someone is handling previous manager -> data
 
-    return allocate_element(pool);
+    return allocate_element(manager);
 }
 
 
@@ -128,7 +128,7 @@ ssize_t destruct_element(struct Ii_manager* manager, struct Ii_element* item) {
         size_t top = (size_t)(manager -> data[i] -> data + manager -> spool_size) - (size_t)item;
 
         if(bottom <= manager -> spool_size * sizeof(struct Ii_element) && top <= manager -> spool_size * sizeof(struct Ii_element)) {
-            pool -> data[i] -> count--;
+            manager -> data[i] -> count--;
             item -> state = EMPTY;
 
             return 0;
@@ -138,17 +138,89 @@ ssize_t destruct_element(struct Ii_manager* manager, struct Ii_element* item) {
     return -1;
 }
 
-
-struct Inverted_index{
+struct Inverted_index {
     struct Ii_manager* manager;
-    struct Ii_element* array;
+    struct Ii_element* array[0];
 };
 
 struct Inverted_index* init_ii(size_t size) {
 
+    size_t alloc_size = sizeof(void*) * size + sizeof(struct Inverted_index);
 
+    struct Inverted_index* ii = (struct Inverted_index*), calloc(1, alloc_size);
+    if(!ii)
+        return (void*)-1;
+
+    ii -> manager = init_ii_m();
+    if(ii -> manager == (void*)-1) {
+        free(ii);
+        return -1;
+    }
+
+    return ii;
 }
 
-ssize_t destruct_ii() {
+ssize_t destruct_ii(struct Inverted_index* ii) {
+    if(!ii)
+        return 0;
 
+    destruct_ii_m(ii -> manager);
+    free(ii);
+
+    return 0;
+}
+
+ssize_t add_item(struct Inverted_index* ii, void* item, size_t start, size_t until){
+    if(!ii)
+        return -1;
+
+    for(size_t i = start; i <= until; i++) {
+        struct Ii_element* tmp = allocate_element(ii -> manager);
+        if(tmp == -1)
+            return i - start;
+
+        tmp -> item = item;
+        tmp -> pos_inside = i - start;
+
+        struct Ii_element* place = ii -> data[i];
+        if(!place) {
+            data[i] = tmp;
+            continue;
+        }
+
+        while(place -> next)
+            place = place -> next;
+
+        place -> next = tmp;
+    }
+
+    return until - start;
+}
+
+ssize_t delete_item(struct Inverted_index* ii, void* item, size_t start, size_t until) {
+    if(ii)
+        return 0;
+
+    for(size_t i = start; i <= until; i++) {
+
+        struct Ii_element* place = ii -> data[i];
+        if(place == item) {
+            data[i] = item -> next;
+            item -> next = 0;
+            item -> pos_inside = 0;
+            continue;
+        }
+
+        while(place -> next != item && place -> next)
+            place = place -> next;
+
+        if(!place -> next)
+            return until - start;
+
+        place -> next = item -> next;
+        item -> next = 0;
+        item -> pos_inside = 0;
+    }
+
+    return until - start;
 }
